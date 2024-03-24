2e077c61bf794fd261dd9c52996ecd2b4493a66f
hiropi1224 <hiropi1224s@gmail.com>
2024-03-25 00:11:23 +0900

    💄 shadcn-ui / button

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import { Button } from "~/components/ui/button";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      Hello, world
-    </main>
+    <div>
+      <Button>Click me</Button>
+    </div>
   );
 }
