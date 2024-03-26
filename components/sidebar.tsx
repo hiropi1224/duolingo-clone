b1c7ec1a7e9e63dd0ee64d6827673b52cc04f2bb
hiropi1224 <hiropi1224s@gmail.com>
2024-03-27 00:22:02 +0900

    💄 sidebar

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,5 +1,9 @@
-"use client";
+import { ClerkLoaded, ClerkLoading, UserButton } from "@clerk/nextjs";
+import { Loader } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
 
+import { SidebarItem } from "~/components/sidebar-item";
 import { cn } from "~/lib/utils";
 
 type Props = {
@@ -10,11 +14,55 @@ export function Sidebar({ className }: Props) {
   return (
     <div
       className={cn(
-        "left-0 top-0 flex h-full flex-col border-r-2 bg-blue-500 px-4 lg:fixed lg:w-[256px]",
-        className,
+        "left-0 top-0 flex h-full flex-col border-r-2 px-4 lg:fixed lg:w-[256px]",
+        className
       )}
     >
-      Sidebar
+      <Link href="/learn">
+        <div className=" flex items-center gap-x-3 pb-7 pl-4 pt-8">
+          <Image src="/mascot.svg" height={40} width={40} alt="Mascot" />
+          <h1 className="text-2xl font-extrabold tracking-wide text-green-600">
+            Lingo
+          </h1>
+        </div>
+      </Link>
+      <div className="flex flex-1 flex-col gap-y-2">
+        {Object.entries(navItem).map(([href, { label, iconSrc }]) => (
+          <SidebarItem
+            key={label}
+            label={label}
+            href={href}
+            iconSrc={iconSrc}
+          />
+        ))}
+      </div>
+      <div className="p-4">
+        <ClerkLoading>
+          <Loader className="size-5 animate-spin text-muted-foreground" />
+        </ClerkLoading>
+        <ClerkLoaded>
+          <UserButton afterSignOutUrl="/" />
+        </ClerkLoaded>
+      </div>
     </div>
   );
 }
+
+const navItem = {
+  "/learn": {
+    label: "Learn",
+    iconSrc: "/learn.svg",
+  },
+  "/leaderboard": {
+    label: "Leaderboard",
+    iconSrc: "/leaderboard.svg",
+  },
+  "/quests": {
+    label: "Quests",
+    iconSrc: "/quests.svg",
+  },
+  "/shop": {
+    label: "Shop",
+    iconSrc: "/shop.svg",
+  },
+};
