8fe4d7d39b0bacc832f6fc6e0a52a8b34a697985
hiropi1224 <hiropi1224s@gmail.com>
2024-03-29 23:28:51 +0900

    🗃️ db schema / queries

--- db/queries.ts
+++ db/queries.ts
@@ -1,6 +1,24 @@
+import { auth } from "@clerk/nextjs";
+import { eq } from "drizzle-orm";
 import { cache } from "react";
 
 import db from "~/db/drizzle";
+import { userProgress } from "~/db/schema";
+
+export const getUserProgress = cache(async () => {
+  const { userId } = auth();
+
+  if (!userId) return null;
+
+  const data = await db.query.userProgress.findFirst({
+    where: eq(userProgress.userId, userId),
+    with: {
+      activeCourse: true,
+    },
+  });
+
+  return data;
+});
 
 export const getCourses = cache(async () => {
   const data = await db.query.courses.findMany();
