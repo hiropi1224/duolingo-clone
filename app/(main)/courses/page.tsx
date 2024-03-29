699c6abc9db7d07e684804b92f01ea7be7b4aa5a
hiropi1224 <hiropi1224s@gmail.com>
2024-03-29 23:56:54 +0900

    💄 courses page

--- app/(main)/courses/page.tsx
+++ app/(main)/courses/page.tsx
@@ -1,13 +1,19 @@
 import { List } from "~/app/(main)/courses/_components/list";
-import { getCourses } from "~/db/queries";
+import { getCourses, getUserProgress } from "~/db/queries";
 
 export default async function CoursesPage() {
-  const courses = await getCourses();
+  const coursesData = getCourses();
+  const userProgressData = getUserProgress();
+
+  const [courses, userProgress] = await Promise.all([
+    coursesData,
+    userProgressData,
+  ]);
 
   return (
     <div className="mx-auto h-full max-w-[912px] px-3">
       <h1 className="text-2xl font-bold text-neutral-700">Language Courses</h1>
-      <List courses={courses} activeCourseId={1} />
+      <List courses={courses} activeCourseId={userProgress?.activeCourseId} />
     </div>
   );
 }
--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,10 +1,20 @@
+import { redirect } from "next/navigation";
+
 import { FeedWrapper } from "~/components/feed-wrapper";
 import { StickyWrapper } from "~/components/sticky-wrapper";
 import { UserProgress } from "~/components/user-progress";
+import { getUserProgress } from "~/db/queries";
 
 import { Header } from "./header";
 
-export default function LearnPage() {
+export default async function LearnPage() {
+  const userProgressData = getUserProgress();
+  const [userProgress] = await Promise.all([userProgressData]);
+
+  if (!userProgress || !userProgress.activeCourse) {
+    redirect("/courses");
+  }
+
   return (
     <div className="flex flex-row-reverse gap-[48px] px-6">
       <StickyWrapper>
