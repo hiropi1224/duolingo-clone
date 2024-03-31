a991987456c0209d7754f14f5145433a34bd3255
hiropi1224 <hiropi1224s@gmail.com>
2024-03-31 19:02:18 +0900

    💄 challenge card

--- app/lesson/_components/quiz.tsx
+++ app/lesson/_components/quiz.tsx
@@ -2,8 +2,10 @@
 
 import { useState } from "react";
 
+import { QuestionBubble } from "~/app/lesson/_components/question-bubble";
 import { challengeOptions, challenges } from "~/db/schema";
 
+import { Challenge } from "./challenge";
 import { Header } from "./header";
 
 type Props = {
@@ -26,6 +28,22 @@ export function Quiz({
 }: Props) {
   const [hearts, setHearts] = useState(initialHearts);
   const [percentage, setPercentage] = useState(initialPercentage);
+  const [challenges] = useState(initialLessonChallenges);
+  const [activeIndex, setActiveIndex] = useState(() => {
+    const uncompletedIndex = challenges.findIndex(
+      (challenge) => !challenge.completed
+    );
+
+    return uncompletedIndex === -1 ? 0 : uncompletedIndex;
+  });
+
+  const challenge = challenges[activeIndex];
+  const options = challenge?.challengeOptions ?? [];
+
+  const title =
+    challenge.type === "ASSIST"
+      ? "Select the correct meaning"
+      : challenge.question;
 
   return (
     <>
@@ -34,6 +52,28 @@ export function Quiz({
         percentage={percentage}
         hasActiveSubscription={!!userSubscription?.isActive}
       />
+      <div className="flex-1">
+        <div className="flex h-full items-center justify-center">
+          <div className="flex w-full flex-col gap-y-12 px-6 lg:min-h-[350px] lg:w-[600px] lg:px-0">
+            <h1 className="text-center text-lg font-bold text-neutral-700 lg:text-start lg:text-3xl">
+              {title}
+            </h1>
+            <div>
+              {challenge.type === "ASSIST" && (
+                <QuestionBubble question={challenge.question} />
+              )}
+              <Challenge
+                options={options}
+                onSelect={() => {}}
+                status="none"
+                selectedOption={undefined}
+                disabled={false}
+                type={challenge.type}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
