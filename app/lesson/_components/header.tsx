1676b64586d9ebf8e86dea55344b0b378ef8bed4
hiropi1224 <hiropi1224s@gmail.com>
2024-03-31 15:44:57 +0900

    💄 lesson page / header

---
+++ app/lesson/_components/header.tsx
@@ -0,0 +1,36 @@
+import { Infinity, X } from "lucide-react";
+import Image from "next/image";
+
+import { Progress } from "~/components/ui/progress";
+
+type Props = {
+  hearts: number;
+  percentage: number;
+  hasActiveSubscription: boolean;
+};
+
+export function Header({ hearts, percentage, hasActiveSubscription }: Props) {
+  return (
+    <header className="mx-auto flex w-full max-w-[1140px] items-center justify-between gap-x-7 px-10 pt-[20px] lg:pt-[50px]">
+      <X
+        onClick={() => {}} // TODO
+        className="cursor-pointer text-slate-500 transition hover:opacity-75"
+      />
+      <Progress value={percentage} />
+      <div className="flex items-center font-bold text-rose-500">
+        <Image
+          src="/heart.svg"
+          alt="Heart"
+          height={28}
+          width={28}
+          className="mr-2"
+        />
+        {hasActiveSubscription ? (
+          <Infinity className="size-6 stroke-[3]" />
+        ) : (
+          hearts
+        )}
+      </div>
+    </header>
+  );
+}
---
+++ app/lesson/page.tsx
@@ -0,0 +1,33 @@
+import { redirect } from "next/navigation";
+
+import { Quiz } from "~/app/lesson/_components/quiz";
+import { getLesson, getUserProgress } from "~/db/queries";
+
+export default async function LessonPage() {
+  const lessonData = getLesson();
+  const userProgressData = getUserProgress();
+
+  const [lesson, userProgress] = await Promise.all([
+    lessonData,
+    userProgressData,
+  ]);
+
+  if (!lesson || !userProgress) {
+    redirect("/learn");
+  }
+
+  const initialPercentage =
+    (lesson.challenges.filter((challenge) => challenge.completed).length /
+      lesson.challenges.length) *
+    100;
+
+  return (
+    <Quiz
+      initialLessonId={lesson.id}
+      initialLessonChallenges={lesson.challenges}
+      initialHearts={userProgress.hearts}
+      initialPercentage={initialPercentage}
+      userSubscription={null}
+    />
+  );
+}
